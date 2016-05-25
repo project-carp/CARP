4f45949f7f5c70d0f8befd4ccb6d45fcb5ccb197
Bartłomiej Mike <Bartlomiej.Mike@ericsson.com>
2016-05-25 12:02:44 +0200

    Gradle tasks configuration

--- src/carp-webapp/src/main/resources/static/app/app.component.js
+++ src/carp-webapp/src/main/resources/static/app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Carpoling in Angular, mmmm, wow ...</h1>'
+            template: '<h1>Carpoling in Angular2, mmmm, wow ...</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
