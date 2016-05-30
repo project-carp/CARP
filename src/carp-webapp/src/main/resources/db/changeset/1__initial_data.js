d331ed98206429a0994ea3f0446816874d5cea0f
Lukasz <kraluk@gmail.com>
2016-05-30 16:18:59 +0200

    Added mongeez (MongoDB migration tool) configuration

---
+++ src/carp-webapp/src/main/resources/db/changeset/1__initial_data.js
@@ -0,0 +1,7 @@
+//mongeez formatted javascript
+//changeset lkra:1__initial_data
+
+// Inserts
+db.users.insert({"userName": "michael_lysaght", "password": "1234"});
+db.users.insert({"userName": "oleksii_iepishkin", "password": "4321"});
+db.users.insert({"userName": "michail_gorbachev", "password": "4321"});
