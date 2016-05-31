213c9e1e66650953ca7cb89fc05f0ee91ec5146f
Grzegorz Rzepka <grze@ericpol.com>
2016-05-31 15:55:09 +0200

    Login page

--- src/carp-webapp/src/main/resources/static/app/login/login.component.ts
+++ src/carp-webapp/src/main/resources/static/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router-deprecated';
+import { Http } from '@angular/http';
+import { contentHeaders } from '../common/headers';
 
 @Component({
     selector: 'login-tag',
@@ -7,7 +9,21 @@ import { Router } from '@angular/router-deprecated';
 })
 
 export class LoginPage {
-    constructor(
-        private router: Router
-    ){}
+    constructor(public router: Router, public http: Http) {
+    }
+
+    login(event, email, password) {
+        event.preventDefault();
+        let body = JSON.stringify({ email, password });
+        this.http.post('http://localhost:18080/login/carp', body, { headers: contentHeaders })
+            .subscribe(
+                response => {
+                    this.router.parent.navigateByUrl('/homePage');
+                },
+                error => {
+                    alert(error.text());
+                    console.log(error.text());
+                }
+            );
+    }
 }
--- src/carp-webapp/src/main/resources/static/app/routing/app.component.ts
+++ src/carp-webapp/src/main/resources/static/app/routing/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core'
 import { LoginPage } from '../login/login.component'
+import { HomePage } from '../home/home.component'
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
 
 @Component({
 	selector: 'main-tag',
 	templateUrl: 'app/routing/main.component.html',
 	directives: [ROUTER_DIRECTIVES],
-	providers: [ROUTER_PROVIDERS]
+	providers: [ROUTER_PROVIDERS, HTTP_PROVIDERS]
 })
 
 @RouteConfig([
@@ -14,6 +16,11 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
 		path: '/loginPage',
 		name: 'LoginPage',
 		component: LoginPage,
+	},
+	{
+		path: '/homePage',
+		name: 'HomePage',
+		component: HomePage,
 	},
 ])
 
