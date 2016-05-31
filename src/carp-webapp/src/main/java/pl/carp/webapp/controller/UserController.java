34007d218c1ef1865b6324ad7717a155fdba877b
Maciej.Iskra <Maciej.Iskra@ericpol.com>
2016-05-31 14:27:02 +0200

    Fixed missing import

--- src/carp-webapp/src/main/java/pl/carp/webapp/controller/UserController.java
+++ src/carp-webapp/src/main/java/pl/carp/webapp/controller/UserController.java
@@ -1,11 +1,6 @@
 package pl.carp.webapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.carp.webapp.model.ApplicationUser;
-import pl.carp.webapp.model.enums.LoginResult;
-import pl.carp.webapp.model.rest.user.LoginRequest;
-import pl.carp.webapp.model.rest.user.LoginResponse;
 
 /**
  * Created by Maciej Iskra(mais) on 2016-05-31.
