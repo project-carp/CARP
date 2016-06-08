df87bcf0244cec0e46f9703e216397272b500e9a
Grzegorz Rzepka <grze@ericpol.com>
2016-06-08 11:56:20 +0200

    Registration and login with sha and createdDate salt

--- src/carp-webapp/src/main/java/pl/carp/webapp/controller/autenthication/RegistrationController.java
+++ src/carp-webapp/src/main/java/pl/carp/webapp/controller/autenthication/RegistrationController.java
@@ -2,10 +2,15 @@ package pl.carp.webapp.controller.autenthication;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pl.carp.webapp.model.entity.ApplicationUser;
 import pl.carp.webapp.model.rest.user.RestResponse;
+import pl.carp.webapp.repository.ApplicationUserRepository;
+
+import java.util.Date;
 
 /**
  * Created by phar on 2016-06-03.
@@ -15,6 +20,8 @@ import pl.carp.webapp.model.rest.user.RestResponse;
 public class RegistrationController {
     private static final Logger log = LoggerFactory.getLogger(RegistrationController.class);
 
+    @Autowired
+    private ApplicationUserRepository applicationUserRepository;
 
     @RequestMapping(value = "/register",
             method = RequestMethod.POST,
@@ -22,7 +29,17 @@ public class RegistrationController {
     public @ResponseBody RestResponse registerUser(@RequestBody ApplicationUser user) {
         log.debug("Attempting to create user by its name '{}'...", user.getUserName());
         RestResponse response = new RestResponse();
-        response.setStatus("ok");
+        if(applicationUserRepository.findByUserName(user.getUserName()) == null) {
+            ShaPasswordEncoder shaPasswordEncoder = new ShaPasswordEncoder();
+            user.setCreatedDate(new Date());
+            user.setPassword(shaPasswordEncoder.encodePassword(user.getPassword(), String.valueOf(user.getCreatedDate().getTime())));
+            applicationUserRepository.save(user);
+            log.debug("User created: '{}'...", user.getUserName());
+            response.setStatus("ok");
+        } else {
+            log.debug("Unable to create user. UserName '{}' exists", user.getUserName());
+            response.setStatus("userName exists");
+        }
         return response;
     }
 
--- src/carp-webapp/src/main/resources/static/app/authentication/login/login.component.ts
+++ src/carp-webapp/src/main/resources/static/app/authentication/login/login.component.ts
@@ -4,14 +4,13 @@ import { Router, Routes, ROUTER_DIRECTIVES } from '@angular/router';
 import { Http } from '@angular/http';
 import {contentHeadersUrlEncoded} from '../../configuration/headers/headers';
 
+declare var Md5:any;
 
 @Component({
     selector: 'login-tag',
     templateUrl: 'app/authentication/login/login.component.html',
     directives: [ROUTER_DIRECTIVES]
 })
-
-
 
 export class LoginPage {
 
@@ -22,7 +21,7 @@ export class LoginPage {
 
     login(event, email, password) {
         event.preventDefault();
-        var body = "email=" + email + "&password=" + password;
+        var body = "email=" + email + "&password=" + Md5.hashStr(password);
         this.http.post(this.loginUrl, body, { headers: contentHeadersUrlEncoded })
             .subscribe(
                 response => {
