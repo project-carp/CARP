dd6baf2b74591d13b82b862b7be6cb3fbdcf7182
Pawel Harla <pawel.harla@ericsson.com>
2016-06-08 13:30:31 +0200

    removed compiled js files

--- src/carp-webapp/src/main/resources/static/dist/configuration/headers/headers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var http_1 = require('@angular/http');
-exports.contentHeaders = new http_1.Headers();
-exports.contentHeaders.append('Accept', 'application/json');
-exports.contentHeaders.append('Content-Type', 'application/json');
-exports.contentHeadersUrlEncoded = new http_1.Headers();
-exports.contentHeadersUrlEncoded.append('Accept', 'application/json');
-exports.contentHeadersUrlEncoded.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
